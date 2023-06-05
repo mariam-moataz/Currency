6db0879af15c91628373c813e342e497f6b94c72
mariam-moataz <mariammansour556@gmail.com>
2023-06-05 23:58:14 +0300

    initial design of ConvertCurrencyVC

--- Currency/View/Controllers/ConvertCurrencyViewController.swift
+++ Currency/View/Controllers/ConvertCurrencyViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConvertCurrencyViewController.swift
 //  Currency
 //
 //  Created by Mariam Moataz on 05/06/2023.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConvertCurrencyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
