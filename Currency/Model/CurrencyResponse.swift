bd7b93fcab066775a3f4b060f78ed71793f00f61
mariam-moataz <mariammansour556@gmail.com>
2023-06-06 03:19:02 +0300

    currency response model

---
+++ Currency/Model/CurrencyResponse.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencyResponse.swift
+//  Currency
+//
+//  Created by Mariam Moataz on 06/06/2023.
+//
+
+import Foundation
+
+struct CurrencyResponse{
+    var success : Bool?
+    var timestamp : Int?
+    var base : String?
+    var date : String?
+    var rates : [String : Double]?
+}
