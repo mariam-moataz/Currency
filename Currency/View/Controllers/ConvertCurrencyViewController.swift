5f4c9e5bd1fe12f582a9fd7fed5ec99ad75255fd
mariam-moataz <mariammansour556@gmail.com>
2023-06-07 06:17:17 +0300

    viewModel and binding data in viewController

--- Currency/View/Controllers/ConvertCurrencyViewController.swift
+++ Currency/View/Controllers/ConvertCurrencyViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import iOSDropDown
+import RxSwift
 
 class ConvertCurrencyViewController: UIViewController {
 
@@ -15,11 +16,19 @@ class ConvertCurrencyViewController: UIViewController {
     @IBOutlet weak var amountTxtField: UITextField!
     @IBOutlet weak var convertedValueTxtField: UITextField!
     
-    
+    let viewModel = CurrencyViewModel()
+    let disposeBag = DisposeBag()
+    var currencies : [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-       // fromDropList.frame(forAlignmentRect: CGRect(x: 110, y: 140, width: 200, height: 30))
-        fromDropList.optionArray = ["1","2","3"]
+        fromDropList.optionArray = ["1","2"]
+        viewModel.currencies.observe(on: MainScheduler.instance).subscribe { [weak self] currencyResponse in
+            guard let rates = currencyResponse.element?.rates else { return }
+            self?.currencies = rates.keys.map {String($0)}
+            self?.fromDropList.optionArray = self?.currencies ?? []
+            //self?.fromDropList.reloadInputViews()
+        }.disposed(by: disposeBag)
+        fromDropList.optionArray = currencies
     }
     
     
@@ -31,3 +40,5 @@ class ConvertCurrencyViewController: UIViewController {
     }
     
 }
+
+
--- Currency/ViewModel/CurrencyViewModel.swift
+++ Currency/ViewModel/CurrencyViewModel.swift
@@ -12,6 +12,6 @@ class CurrencyViewModel{
     let currencies: Observable<CurrencyResponse>
         
     init() {
-        currencies = NetworkService.shared.fetchData(url: "").observe(on: MainScheduler.instance).catchAndReturn(CurrencyResponse())
+        currencies = NetworkService.shared.fetchData(url: "http://data.fixer.io/api/latest?access_key=48077b55b55d82ccb7c21382a249b725").observe(on: MainScheduler.instance).catchAndReturn(CurrencyResponse())
     }
 }
